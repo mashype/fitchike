69896dcb63583fad12f2fbe5c5b43171398cf369
mashype <mashype@me.com>
2016-03-15 08:24:03 -0500

    add the paperclip function

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,30 +1,23 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /profiles
-  # GET /profiles.json
   def index
     @profiles = Profile.all
   end
 
-  # GET /profiles/1
-  # GET /profiles/1.json
   def show
   end
 
-  # GET /profiles/new
   def new
-    @profile = Profile.new
+    @profile = current_user.profiles.build
   end
 
-  # GET /profiles/1/edit
   def edit
   end
 
-  # POST /profiles
-  # POST /profiles.json
   def create
-    @profile = Profile.new(profile_params)
+    @profile = current_user.profiles.build(profile_params)
 
     respond_to do |format|
       if @profile.save
@@ -37,8 +30,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /profiles/1
-  # PATCH/PUT /profiles/1.json
   def update
     respond_to do |format|
       if @profile.update(profile_params)
@@ -51,8 +42,6 @@ class ProfilesController < ApplicationController
     end
   end
 
-  # DELETE /profiles/1
-  # DELETE /profiles/1.json
   def destroy
     @profile.destroy
     respond_to do |format|
@@ -62,13 +51,11 @@ class ProfilesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_profile
       @profile = Profile.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:user_id, :bio, :zip, :gender_id)
+      params.require(:profile).permit(:user_id, :bio, :zip, :avatar, :gender_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315045309) do
+ActiveRecord::Schema.define(version: 20160315125850) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "title"
@@ -35,9 +35,13 @@ ActiveRecord::Schema.define(version: 20160315045309) do
     t.integer  "user_id"
     t.text     "bio"
     t.string   "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "gender_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "profiles", ["gender_id"], name: "index_profiles_on_gender_id"
