b384bb51a5c6546f334dbddd7f13a73326b94f25
mashype <mashype@me.com>
2016-03-15 19:22:26 -0500

    adding PostGres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160315195623) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
     t.string   "title"
     t.text     "comments"
@@ -45,7 +48,7 @@ ActiveRecord::Schema.define(version: 20160315195623) do
     t.datetime "avatar_updated_at"
   end
 
-  add_index "profiles", ["gender_id"], name: "index_profiles_on_gender_id"
+  add_index "profiles", ["gender_id"], name: "index_profiles_on_gender_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
@@ -71,7 +74,8 @@ ActiveRecord::Schema.define(version: 20160315195623) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "profiles", "genders"
 end
