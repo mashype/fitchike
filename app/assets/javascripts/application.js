3b02ae948cc587bd499da39a77ad0727009f5f09
mashype <mashype@me.com>
2016-04-18 19:26:30 -0500

    correction to try and fix gmaps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require bootstrap-sprockets
 //= require masonry/jquery.masonry
@@ -17,5 +18,4 @@
 //= require cocoon
 //= require turbolinks
 //= require underscore
-//= require gmaps/google
 //= require_tree .
