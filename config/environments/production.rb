9304c3bee0fce19b17d34f5331c55477ba89fdec
mashype <mashype@me.com>
2016-09-07 21:18:06 -0700

    fixing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,6 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
 
   # needed for url mailing
   # Change email delivery to :smtp, :sendmail, :file, :test
@@ -77,8 +76,7 @@ Rails.application.configure do
     user_name:            ENV["FMAIL_USERNAME"],
     password:             ENV["FMAIL_PASSWORD"],
     authentication:       'plain',
-    enable_starttls_auto: true  
-  }
+    enable_starttls_auto: true  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
