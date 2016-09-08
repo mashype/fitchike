ec0285f7aa20aa6b7052a399ded2011774457c3c
mashype <mashype@me.com>
2016-09-07 21:13:37 -0700

    fixing mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,21 +65,20 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
 
-  # Change email delivery to :smtp, :sendmail, :file, :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost" }
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "fitchike.com",
-      authentication: "plain",
-      enable_starttle_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
-  }
-
   # needed for url mailing
-  
+  # Change email delivery to :smtp, :sendmail, :file, :test
+#  config.action_mailer.default_url_options = { host: "localhost" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    tls: true,
+    address:              ENV["EMAIL_URL"],
+    port:                 465,
+    domain:               ENV["EMAIL_DOMAIN"],
+    user_name:            ENV["FMAIL_USER_NAME"],
+    password:             ENV["FMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
